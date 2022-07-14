402f3c97531b570c06d4e72b94227030580c1bd7
Katy Brown <katybr35@gmail.com>
2022-07-14 18:20:21 -0500

    trying to get dist folder to create

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,21 +17,23 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate','readwrite');
   const store = tx.objectStore('jate');
-  const result = await store.put({ id: 1, value: content});
-
-  return result;
+  const request =  store.put({ id: 1, value: content});
+  const result =await request.value
+  console.log("Saved to IDB",result)
+  return insertedData;
   
-console.error('putDb not implemented');
+// console.error('putDb not implemented');
 }
  
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.error('getDb not implemented');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
 const request = store.getAll();
 const result = await request;
 console.log(result);
-
+}
 initdb();
